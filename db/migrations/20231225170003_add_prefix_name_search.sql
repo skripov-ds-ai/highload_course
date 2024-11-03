8c2b59564522ec643475716fdc980e695f4dfb38
Denis Skripov <nizhikebinesi@gmail.com>
2024-11-03 21:25:30 +0600

    hw2: fix migration on index creating

--- db/migrations/20231225170003_add_prefix_name_search.sql
+++ db/migrations/20231225170003_add_prefix_name_search.sql
@@ -1,10 +1,10 @@
 -- +goose Up
 -- +goose StatementBegin
-create index if not exists public.users.second_first_name_btree_idx
+create index if not exists second_first_name_btree_idx
     on public.users (second_name varchar_pattern_ops, first_name varchar_pattern_ops);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-drop index if exists public.users.second_first_name_btree_idx;
+drop index if exists public.second_first_name_btree_idx;
 -- +goose StatementEnd
